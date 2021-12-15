635254b2c0086900db1beeb2fda707602ca4b0a8
TuanHuynh <tytuan2504nv@example.com>
2021-12-15 21:34:31 +0700

    add 2 new file

---
+++ circumference-and-area-of-a-circle.js
@@ -0,0 +1,8 @@
+let R = prompt("Nhập bán kính hình tròn: ")
+let pi = 3.14
+    C = 2 * R * pi
+document.write("Chu vi hình tròn = " + C)
+document.write("<br>")
+    S = R^2 * pi
+document.write("Diện tích hình tròn = " + C)
+
