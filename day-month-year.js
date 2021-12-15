247eaa7bd60500dcbdcb2e6219bb950b392149c8
TuanHuynh <tytuan2504nv@example.com>
2021-12-15 21:31:13 +0700

    add 2 new file

---
+++ day-month-year.js
@@ -0,0 +1,5 @@
+var day = prompt("Nhập ngày");
+var month = prompt("Nhập tháng");
+var year = prompt("Nhập năm");
+ddmmyyyyy = day + "-" + month + "-" + year
+document.write("Thời gian: " + ddmmyyyyy);
